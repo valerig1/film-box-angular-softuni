da2c11e3b2e282b10af20299e4eded36a6771ba6
valerig1 <valerig742@gmail.com>
2026-04-03 18:45:51 +0300

    Create main layout structure in app component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer, Header } from './shared/components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

c9c45403db098c9cd6538c4ac8c854af3e06262b
valerig1 <valerig742@gmail.com>
2026-04-05 19:36:02 +0300

    Update login component
    Implements reactive form validation and submission handling

--- client/src/app/features/auth/login/login.ts
+++ client/src/app/features/auth/login/login.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services';
+import { Router, RouterLink } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  protected authService = inject(AuthService);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.loginForm.get('password');
+  }
+
+  get showEmailError(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get showPasswordError(): boolean {
+    return this.password?.invalid && (this.password?.dirty || this.password?.touched) || false;
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!';
+    }
+
+    if (this.email?.errors?.['email']) {
+      return 'Email is not valid!';
+    }
+
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 6 characters!';
+    }
+
+    return '';
+  }
+
+  onSubmit(): void {;
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          alert(err.error.message);
+          this.loginForm.reset();   
+          this.markFormGroupTouched();
+        }
+      });
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    })
+  }
+
+}

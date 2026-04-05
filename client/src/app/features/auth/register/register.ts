7fd826cb04720852b39e0d1405e84511851ae47f
valerig1 <valerig742@gmail.com>
2026-04-05 20:10:00 +0300

    Update register component
    Implements registration form with reactive validation and submission handling

--- client/src/app/features/auth/register/register.ts
+++ client/src/app/features/auth/register/register.ts
@@ -1,10 +1,167 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      passwords: this.formBuilder.group({
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        rePassword: ['', [Validators.required, Validators.minLength(6)]]
+      }, { validators: this.passwordMatchValidator })
+    })
+  }
+
+  get username(): AbstractControl<any, any> | null {
+    return this.registerForm.get('username');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.registerForm.get('email');
+  }
+
+  get passwords(): FormGroup<any> {
+    return this.registerForm.get('passwords') as FormGroup;
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.passwords.get('password');
+  }
+
+  get rePassword(): AbstractControl<any, any> | null {
+    return this.passwords.get('rePassword');
+  }
+
+  get showUsernameError(): boolean {
+    return this.username?.invalid && (this.username?.dirty || this.username?.touched) || false;
+  }
+
+  get showEmailError(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get showPasswordError(): boolean {
+    return this.passwords?.invalid && (this.passwords?.dirty || this.passwords?.touched) || false;
+  }
+
+  get usernameErrorMessage(): string {
+    if (this.username?.errors?.['required']) {
+      return 'Username is required!';
+    }
+
+    if (this.username?.errors?.['minlength']) {
+      return 'Username should have at least 5 characters!';
+    }
+
+    return '';
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!';
+    }
+
+    if (this.email?.errors?.['email']) {
+      return 'Email is not valid!';
+    }
+
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 6 characters!';
+    }
+
+    if (this.passwords?.errors?.['passwordMismatch']) {
+      return 'Passwords do not match!';
+    }
+
+    return '';
+  }
+
+  get rePasswordErrorMessage(): string {
+    if (this.rePassword?.errors?.['required']) {
+      return 'Confirm password is required!';
+    }
+
+    if (this.rePassword?.errors?.['minlength']) {
+      return 'Confirm password must be at least 6 characters!';
+    }
+
+    if (this.passwords?.errors?.['passwordMismatch']) {
+      return 'Passwords do not match!';
+    }
+
+    return '';
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const { username, email } = this.registerForm.value;
+      const { password, rePassword } = this.registerForm.value.passwords;
+
+      this.authService.register(
+        username,
+        email,
+        password,
+        rePassword).subscribe({
+          next: () => {
+            this.router.navigate(['/home']);
+          },
+          error: (err) => {
+            alert(err.error.message);
+            this.registerForm.reset(); 
+            this.markFormGroupTouched();
+          }
+        })
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched(): void {
+    Object.keys(this.registerForm.controls).forEach(key => {
+      const control = this.registerForm.get(key);
+
+      if (control instanceof FormGroup) {
+        Object.keys(control.controls).forEach(nestedKey => {
+          const nestedControl = control.get(nestedKey)
+          nestedControl?.markAllAsTouched();
+        })
+      } 
+      
+      control?.markAsTouched();
+    })
+  }
+
+  private passwordMatchValidator(passwordsControl: AbstractControl): ValidationErrors | null {
+    const password = passwordsControl.get('password');
+    const rePassword = passwordsControl.get('rePassword');
+
+    if (password && rePassword && password.value !== rePassword.value) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
+}

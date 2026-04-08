de81a2f081b3f0a4e5d0210eeb148f1f015ae4c3
valerig1 <valerig742@gmail.com>
2026-04-08 16:09:32 +0300

    Update films service
    Add getFilmById and createFilm methods to FilmsService

--- client/src/app/core/services/films.service.ts
+++ client/src/app/core/services/films.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Film } from "../../models";
+import { AuthService } from "./auth.service";
 
 @Injectable({
     providedIn: 'root'
@@ -9,6 +10,7 @@ import { Film } from "../../models";
 
 export class FilmsService {
     private apiUrl = 'http://localhost:3030/data/films';
+    private authService = inject(AuthService);
 
     constructor(private httpClient: HttpClient) {}
 
@@ -16,7 +18,25 @@ export class FilmsService {
         return this.httpClient.get<Film[]>(this.apiUrl);
     }
 
+    getFilmById(id: string): Observable<Film> {
+        return this.httpClient.get<Film>(`${this.apiUrl}/${id}`);
+    }
+
     getLatestFilms(): Observable<Film[]> {
         return this.httpClient.get<Film[]>(`${this.apiUrl}?sortBy=_createdOn%20desc&pageSize=5`);
+    }
+
+    createFilm(
+        title: string, year: number, genre: string, img: string, description: string): Observable<Film[]> {
+        const token = this.authService.getAccessToken();
+
+        if (!token) {
+            throw new Error("User not authenticated");
+        }
+
+        return this.httpClient.post<Film[]>(
+            this.apiUrl,
+            { title, year, genre, img, description },
+            { headers: { 'X-Authorization': token } });
     }
 }

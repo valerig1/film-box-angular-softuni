8d34fd2b079641327f0049cbf58462e438de9ce9
valerig1 <valerig742@gmail.com>
2026-04-08 16:23:02 +0300

    Update app-routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then(c => c.Home)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then(c => c.Login)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then(c => c.Register)
+    },
+    {
+        path: 'films/create',
+        loadComponent: () => import('./features/films/create-film/create-film').then(c => c.CreateFilm)
+    },
+];
